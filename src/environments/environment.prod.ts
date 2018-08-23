052b63c498d37327cdc36ac5b6648c3ea6e53faa
Brenda <lezti.bre89@gmail,com>
2018-08-23 12:45:35 -0500

    Firebase Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAMPnNC4i9ab80PopBGaWQT2A6PtwuTC5s",
+    authDomain: "creacion1-a4c2c.firebaseapp.com",
+    databaseURL: "https://creacion1-a4c2c.firebaseio.com",
+    projectId: "creacion1-a4c2c",
+    storageBucket: "creacion1-a4c2c.appspot.com",
+    messagingSenderId: "847307855903"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyAMPnNC4i9ab80PopBGaWQT2A6PtwuTC5s",
+    authDomain: "creacion1-a4c2c.firebaseapp.com",
+    databaseURL: "https://creacion1-a4c2c.firebaseio.com",
+    projectId: "creacion1-a4c2c",
+    storageBucket: "creacion1-a4c2c.appspot.com",
+    messagingSenderId: "847307855903"
+  }
 };
